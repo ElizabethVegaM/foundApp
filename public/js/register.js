db57be0795b069624565451564efb029a2d2004a
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-10-03 19:16:50 -0300

    trabajando en formularios de persona y objetos

---
+++ public/js/register.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+
+}
+
+const showPersonForm = () => {
+  objectContainer.style.display = 'none';
+  personContainer.style.display = 'block';
+}
+
+const showObjectForm = () => {
+  personContainer.style.display = 'none';
+  objectContainer.style.display = 'block';
+}
+
