a60511f23adc28471ac6471d8da5e412c6e6c06c
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-10-03 20:23:06 -0300

    formulario de registro de objetos en firebase

--- public/js/register.js
+++ public/js/register.js
@@ -40,6 +40,7 @@ document.getElementById('personForm').addEventListener('submit', (event) => {
     let pInfo = extraInfo.value;
     let finder = personFinder.value;
     let pPhone = personPhone.value;
+
     const newUserKey = firebase.database().ref().child('foundPerson').push().key;
     firebase.database().ref(`foundPerson/${newUserKey}`).set({
       name: pName,
@@ -52,13 +53,43 @@ document.getElementById('personForm').addEventListener('submit', (event) => {
       finderPhone: pPhone,
       latitude: latitude,
       longitude: longitude
-      //photo: photoURL
     });
   });
 });
 
-const addObject = (event) => {
+document.getElementById('objectPhoto').addEventListener('change', function(event) {
+  let storageRef = firebase.storage().ref().child(`objectPhoto/a${personPhone.value}.jpeg`);
+  let firstFile = event.target.files[0];
+  storageRef.put(firstFile);
+  storageRef.getDownloadURL().then(function(url) {
+    personPhotoMessage.innerHTML = '¡Listo!';
+    photoURL = url;
+    console.log(photoURL);
+  }).catch(function(error) {
+    console.log('Ha ocurrido un error' + error);
+  });
+});
+
+document.getElementById('objectForm').addEventListener('submit', (event) => {
   event.preventDefault();
-}
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    let oType = objectType.value;
+    let oInfo = objectInfo.value;
+    let oFinder = objectFinder.value;
+    let oPhone = objectPhone.value;
+    const newUserKey = firebase.database().ref().child('foundObjects').push().key;
+    firebase.database().ref(`foundObjects/${newUserKey}`).set({
+      type: oType,
+      description: oInfo,
+      finderName: oFinder,
+      finderphone: oPhone,
+      latitude: latitude,
+      longitude: longitude
+    });
+  })
+});
 
 
