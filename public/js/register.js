ff4edb5f9867f934f3e99d619d7fc1a32fb9d704
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-10-03 20:14:37 -0300

    subiendo datos de formulario (personas) a firebase, con ubicación incluida

--- public/js/register.js
+++ public/js/register.js
@@ -12,3 +12,53 @@ const showObjectForm = () => {
   objectContainer.style.display = 'block';
 }
 
+let photoURL;
+document.getElementById('personPhoto').addEventListener('change', function(event) {
+  let storageRef = firebase.storage().ref().child(`personPhoto/a${personPhone.value}.jpeg`);
+  let firstFile = event.target.files[0];
+  storageRef.put(firstFile);
+  storageRef.getDownloadURL().then(function(url) {
+    personPhotoMessage.innerHTML = '¡Listo!';
+    photoURL = url;
+    console.log(photoURL);
+  }).catch(function(error) {
+    console.log('Ha ocurrido un error' + error);
+  });
+});
+
+document.getElementById('personForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    let pName = foundName.value;
+    let pGender = gender.value;
+    let pAge = age.value;
+    let pHeight = personHeight.value;
+    let pEye = eye.value;
+    let pInfo = extraInfo.value;
+    let finder = personFinder.value;
+    let pPhone = personPhone.value;
+    const newUserKey = firebase.database().ref().child('foundPerson').push().key;
+    firebase.database().ref(`foundPerson/${newUserKey}`).set({
+      name: pName,
+      gender: pGender,
+      age: pAge,
+      height: pHeight,
+      eyeColor: pEye,
+      description: pInfo,
+      finderName: finder,
+      finderPhone: pPhone,
+      latitude: latitude,
+      longitude: longitude
+      //photo: photoURL
+    });
+  });
+});
+
+const addObject = (event) => {
+  event.preventDefault();
+}
+
+
