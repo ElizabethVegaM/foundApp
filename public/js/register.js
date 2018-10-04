00a8fc31e38d91b109d1c7ecdf63e200cc39ffc7
ElizabethVegaM <eb.vega.m@gmail.com>
2018-10-04 12:52:15 -0300

    ficha completa

--- public/js/register.js
+++ public/js/register.js
@@ -12,6 +12,11 @@ const showObjectForm = () => {
   objectContainer.style.display = 'block';
 }
 
+const backToForm = () => {
+  newEntry.style.display = 'none';
+  forms.style.display = 'flex';
+}
+
 let photoURL;
 document.getElementById('personPhoto').addEventListener('change', function(event) {
   let storageRef = firebase.storage().ref().child(`personPhoto/a${personPhone.value}.jpeg`);
@@ -26,25 +31,28 @@ document.getElementById('personPhoto').addEventListener('change', function(event
   });
 });
 
+let latitude;
+let longitude;
 document.getElementById('personForm').addEventListener('submit', (event) => {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(function(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
-    let pName = foundName.value;
     let pGender = gender.value;
     let pAge = age.value;
     let pHeight = personHeight.value;
     let pEye = eye.value;
+    let pHair = hair.value;
     let pInfo = extraInfo.value;
     let finder = personFinder.value;
     let pPhone = personPhone.value;
 
+    forms.style.display = 'none';
+
     const newUserKey = firebase.database().ref().child('found').push().key;
     firebase.database().ref(`found/${newUserKey}`).set({
-      option: person,
-      name: pName,
+      option: 'person',
       gender: pGender,
       age: pAge,
       height: pHeight,
@@ -55,6 +63,41 @@ document.getElementById('personForm').addEventListener('submit', (event) => {
       latitude: latitude,
       longitude: longitude
     });
+
+    newEntry.innerHTML = `<div class="entry-container">
+      <div class="entry-photo">
+        <img src="img/facebook-blank-face-blank-300x225-300x225-1.jpeg" alt="">
+      </div>
+      <h4>${pGender}</h4>
+      <p>${pInfo}</p>
+      <div class="entryInfo row">
+        <div class="col-6">
+          <p class="info-title"">Edad</p>
+          <p>${pAge}</p>
+          <p class="info-title">Ojos</p>
+          <p>${pEye}</p>
+        </div>
+        <div class="col-6">
+          <p class="info-title">Estatura</p>
+          <p>${pHeight}</p>
+          <p class="info-title">Cabello</p>
+          <p>${pHair}</p>
+        </div>
+      </div>
+      <div class="finderEntry row">
+        <div class="col-6">
+          <p class="info-title">Nombre Contacto</p>
+          <p>${finder}</p>
+        </div>
+        <div class="col-6">
+          <p class="info-title">Teléfono</p>
+          <p>+569 ${pPhone}</p>        
+        </div>
+      </div>
+      <div class="submit-container">
+        <button type="button" class="btn option-btn" onclick="backToForm()"><span><i class="fas fa-caret-left"></i></span> Volver</button>
+        <a href="home.html" type="button" class="btn selected-option" onclick="registerFirebase()">Registrar <span><i class="fas fa-caret-right"></i></span></a>
+      </div>`;
   });
 });
 
@@ -74,16 +117,19 @@ document.getElementById('objectPhoto').addEventListener('change', function(event
 document.getElementById('objectForm').addEventListener('submit', (event) => {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(function(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
     let oType = objectType.value;
     let oInfo = objectInfo.value;
     let oFinder = objectFinder.value;
     let oPhone = objectPhone.value;
+
+    forms.style.display = 'none';
+
     const newUserKey = firebase.database().ref().child('found').push().key;
     firebase.database().ref(`found/${newUserKey}`).set({
-      option:object,
+      option: 'object',
       type: oType,
       description: oInfo,
       finderName: oFinder,
@@ -91,7 +137,37 @@ document.getElementById('objectForm').addEventListener('submit', (event) => {
       latitude: latitude,
       longitude: longitude
     });
+
+    newEntry.innerHTML = `<div class="entry-container">
+      <div class="entry-photo">
+        <img src="img/wallet.jpg" alt="">
+      </div>
+      <h4>${oType}</h4>
+      <div class="entryInfo">
+        <p>${oInfo}</p>
+        <div class="finderEntry row">
+          <div class="col-6">
+            <p class="info-title">Nombre Contacto</p>
+            <p>${oFinder}</p>  
+          </div>
+          <div class="col-6">
+            <p class="info-title">Teléfono</p>
+            <p>+569 ${oPhone}</p>
+          </div>
+        </div>
+      </div>
+      <div class="submit-container">
+        <button type="button" class="btn option-btn" onclick="backToForm()"><span><i class="fas fa-caret-left"></i></span> Volver</button>
+        <a href="home.html" type="button" onclick="registerFirebase()" class="btn selected-option">Registrar <span><i class="fas fa-caret-right"></i></span></a>
+      </div>
+    </div>`;
   })
 });
+
+const registerFirebase = () => {
+  console.log('Registro Exitoso');
+  window.location = '../home.html';
+};
+
 
 module.exports=register
