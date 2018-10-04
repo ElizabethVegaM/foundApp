6f795936698618d468384deb2c92495c24944324
antonella <antonella_caroca@hotmail.com>
2018-10-04 00:48:55 -0300

    agrego mapa que detecta ubicación de usurio según sus coordenadas

--- search/search.js
+++ search/search.js
@@ -1,76 +1,47 @@
+let platform = new H.service.Platform({ // se iniciliza el mapa y se ingresan credenciales
+  app_id: '3imMONAvt7Aj1IMBu4Lt',
+  app_code: 'lEToiO4p-ytdOVw1Sf9iyw',
+  useCIT: true
+});
 
-/**
- * Adds a  draggable marker to the map..
- *
- * @param {H.Map} map                      A HERE Map instance within the
- *                                         application
- * @param {H.mapevents.Behavior} behavior  Behavior implements
- *                                         default interactions for pan/zoom
- */
-function addDraggableMarker(map, behavior){
+let defaultLayers = platform.createDefaultLayers();
+let mapContainer = document.getElementById('mapContainer');
 
-    var marker = new H.map.Marker({lat:-33.4167, lng:-70.65});
-    // Ensure that the marker can receive drag events
-    marker.draggable = true;
-    map.addObject(marker);
-  
-    // disable the default draggability of the underlying map
-    // when starting to drag a marker object:
-    map.addEventListener('dragstart', function(ev) {
-      var target = ev.target;
-      if (target instanceof H.map.Marker) {
-        behavior.disable();
-      }
-    }, false);
-  
-  
-    // re-enable the default draggability of the underlying map
-    // when dragging has completed
-    map.addEventListener('dragend', function(ev) {
-      var target = ev.target;
-      if (target instanceof mapsjs.map.Marker) {
-        behavior.enable();
-      }
-    }, false);
-  
-    // Listen to the drag event and move the position of the marker
-    // as necessary
-     map.addEventListener('drag', function(ev) {
-      var target = ev.target,
-          pointer = ev.currentPointer;
-      if (target instanceof mapsjs.map.Marker) {
-        target.setPosition(map.screenToGeo(pointer.viewportX, pointer.viewportY));
-      }
-    }, false);
-  }
-  
-  /**
-   * Boilerplate map initialization code starts below:
-   */
-  
-  //Step 1: initialize communication with the platform
-  var platform = new H.service.Platform({
-    app_id: 'YTwDUxFfBXp7TI2GimXS',
-    app_code: 'qlKZLMttFtDmjErf3WAolA',
-    useCIT: true,
-    useHTTPS: true
+//el mapa se adapta al cambio  de tamaño de la ventana
+window.addEventListener('resize', function(){
+    map.getViewPort().resize();
   });
-  var defaultLayers = platform.createDefaultLayers();
+
+var coordinates = {
+  lat: -33.43727, // coordenadas santiago de chile
+  lng: -70.65056
+};
+let mapOptions = {
+  center: coordinates,
+  zoom: 15
+};
+
+let map = new H.Map(
+  mapContainer,
+  defaultLayers.normal.map,
+  mapOptions
+);
+let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+let HEREHQcoordinates;
+
+//acá se actualizan las coordenadas
+function updatePosition (event) {
+  HEREHQcoordinates = {
+    lat: event.coords.latitude,
+    lng: event.coords.longitude
+  };
+  console.log(HEREHQcoordinates);
   
-  //Step 2: initialize a map - this map is centered over Boston
-  var map = new H.Map(document.getElementById('map'),
-    defaultLayers.normal.map,{
-    center: {lat:-33.4167, lng:-70.65},
-    zoom: 14
-  });
-  
-  //Step 3: make the map interactive
-  // MapEvents enables the event system
-  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-  
-  // Step 4: Create the default UI:
-  var ui = H.ui.UI.createDefault(map, defaultLayers, 'en-US');
-  
-  // Add the click event listener.
-  addDraggableMarker(map, behavior);
+  var marker = new H.map.Marker(HEREHQcoordinates);
+  map.addObject(marker);
+  map.setCenter(HEREHQcoordinates);
+}
+// watchPosition devuelve la ubicación actual del usuario y actualiza la ubicación según el desplazamiento del usuario
+navigator.geolocation.watchPosition(updatePosition);
+
