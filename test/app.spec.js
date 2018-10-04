99db4a883a46e1e55529043c8f173c951dbb9f8b
Mia5391 <mariakrasteva5391@gmail.com>
2018-10-04 08:55:44 -0300

    app tests added

---
+++ test/app.spec.js
@@ -0,0 +1,15 @@
+describe('fadeout', () => {
+
+  it('should be a function', () => {
+    assert.equal(typeof fadeout, 'function');
+  });
+
+});
+
+describe('disappear', () => {
+
+  it('should be a function', () => {
+    assert.equal(typeof disappear, 'function');
+  });
+
+});
